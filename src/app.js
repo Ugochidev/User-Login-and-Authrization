e62716feb6fa6e94d84d865e9a4399f7117dbdff
temitopeAdeyemo <temitopejulius9@gmail.com>
2022-02-20 12:49:59 +0100

    I just built endpoints for user registration and login

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 require("./database/index")
 require("./auth/passport");
-
+const connectDB = require("./database/index")
 require("./models/user");
 const middlewares = require("./middlewares");
 const api = require("./api");
@@ -12,16 +12,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-const connectDB = async ()=>{
-    try {
-        await mongoose.connect(process.env.DATA_URI)
-                console.log(`Database connected`)
-    } catch (error) {
-        console.log(`Database is not connected`)
-    }
-}
 connectDB()
-
 app.use("/api/v1", api);
 
 app.listen(PORT, ()=>{
